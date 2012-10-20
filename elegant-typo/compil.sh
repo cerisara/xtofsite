1644a7fc9804b313de7778784d6beb0a5b503ee0
xtoff <cerisara@loria.fr>
2012-10-20 11:12:17 +0200

    ok

--- elegant-typo/compil.sh
+++ elegant-typo/compil.sh
@@ -4,7 +4,7 @@ cd jcompil
 java -cp bin jcompil.JCompil
 
 cd ..
-foreach (index jobs links jsafran software jtrans research news go publis)
+foreach i (index jobs links jsafran software jtrans research news go publis)
   iconv --from=UTF-8 --to=ISO-8859-1 $i".html" | sed 's,UTF-8,ISO-8859-1,g' >! ~/web/$i".html"
 end
 
