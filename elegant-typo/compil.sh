07cb13f95ef9cc7494674d1d505e29a38f457017
Christophe Cerisara <cerisara@biber.(none)>
2012-10-20 09:54:34 +0200

    ok

--- elegant-typo/compil.sh
+++ elegant-typo/compil.sh
@@ -3,3 +3,13 @@
 cd jcompil
 java -cp bin jcompil.JCompil
 
+cp index.html ~/web/
+cp jobs.html ~/web/
+cp links.html ~/web/
+cp software.html ~/web/
+cp jsafran.html ~/web/
+cp research.html ~/web/
+cp news.html ~/web/
+cp go.html ~/web/
+cp publis.html ~/web/
+
