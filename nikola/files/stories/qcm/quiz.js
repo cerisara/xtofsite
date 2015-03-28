920245f63ee4d0cd32610cd8c90743e04a6aeed9
xtof <cerisara@loria.fr>
2015-03-28 19:15:33 +0100

    bugfix

--- nikola/files/stories/qcm/quiz.js
+++ nikola/files/stories/qcm/quiz.js
@@ -1,14 +1,22 @@
 
 function gradeit(){
+	var username=eval("document.myquiz.username").value
 for (q=0;q<totalquestions;q++){
 	var thequestion=eval("document.myquiz.question"+q)
+		   actualchoices[q]="unanswered"
 	for (c=0;c<thequestion.length;c++){
 		if (thequestion[c].checked==true)
 		   actualchoices[q]=thequestion[c].value
-		else
-		   actualchoices[q]="unanswered"
 	}
 }
+  var xxml='<user>'+username+'</user>\n';
+  xxml+='<reps>\n';
+  for (q=0;q<totalquestions;q++) {
+    xxml+='<q>'+q+'</q>';
+    xxml+='<r>'+actualchoices[q]+'</r>\n';
+  }
+  xxml+='</reps>\n';
+  $.post('http://127.0.0.1/xtof/siteperso/repqcm.php', {xml: xxml });
 
 // TODO: send the actualchoices array by email
 
